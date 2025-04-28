3988a77605da0cab85620b22837c9c0d2169f6fe
Shehab Elkhaligy <shehabelkhaligy100@gmail.com>
2025-04-28 05:39:12 +0300

    Modified ProductImage Table To Handle ImagePaths Instead Of Rawbytes and Applied Db Migraions

---
+++ Project/Migrations/20250428013536_AddImagePathColumn.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Project.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddImagePathColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageData",
+                table: "ProductImages");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImagePath",
+                table: "ProductImages",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImagePath",
+                table: "ProductImages");
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "ImageData",
+                table: "ProductImages",
+                type: "varbinary(max)",
+                nullable: false,
+                defaultValue: new byte[0]);
+        }
+    }
+}
--- Project/Models/ProductImage.cs
+++ Project/Models/ProductImage.cs
@@ -1,15 +1,19 @@
 ﻿namespace Project.Models
 {
     public class ProductImage
-    {   
+    {
         public int Id { get; set; }
         public int ProductId { get; set; }
         public virtual Product? Product { get; set; }
         // The actual image data stored as byte array
-        public required byte[] ImageData { get; set; }
+        // public required byte[] ImageData { get; set; }
+
 
         // Computed property to convert byte array to Base64 string
-        public string? ImageBase64 => ImageData != null ? $"data:image/png;base64,{Convert.ToBase64String(ImageData)}" : null;
-            
+        // public string? ImageBase64 => ImageData != null ? $"data:image/png;base64,{Convert.ToBase64String(ImageData)}" : null;
+
+        // This is the image path on my server
+        // It will be something like that /images/products/product1.jpg
+        public required string ImagePath { get; set; }
     }
 }
